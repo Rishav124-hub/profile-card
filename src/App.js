e6144aec84a3fb751fa593cc61eb335e6b9812c5
Rishav Jaiswal <rishav.rj41@gmail.com>
2021-01-06 13:08:31 +0530

    changing in app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import React ,{ Component } from "react";
+import logo from './img2.png';
+import './img1.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <div className="profile-card">
+        <div className="top-section">
+            <div className="img">
+              <img src={logo} width="500" alt="logo"></img>
+              <div className="name">Rishav Jaiswal</div>
+            </div>
+        </div>
+        <div className="bottom-section">
+            <div className="icon-part">
+                <a href="https://www.linkedin.com/in/rishav-jaiswal-28976b17b/" className="icon"><span className="fa fa-linkedin-square" ></span></a>
+                <a href="https://github.com/Rishav124-hub" className="icon"><span className="fa fa-github-square" ></span></a>
+                <a href="https://twitter.com/RishavJ16408127" className="icon"><span className="fa fa-twitter-square" ></span></a>
+                <a href="https://www.instagram.com/rishav_jaiswal_rj/" className="icon"><span className="fa fa-instagram"></span></a>
+            </div>
+            <div className="content">
+                <span className="about">
+                    Hello Friends!.
+                    My name is Rishav Jaiswal.
+                    I am B.Tech CSE student of Sanskriti University
+                    And also a Full Stack learner with upGrad. 
+                    And I have completed a lot of basic projects in web design. 
+                    I also completed my Data Science certification course with upGrad.
+                </span>
+            </div>
+        </div>
     </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default App;
